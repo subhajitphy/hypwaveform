846b89f9a0609cde0a891ad72b39251328ca551b
Subhajit <107269864+subhajitphy@users.noreply.github.com>
2022-08-08 15:46:30 +0200

    Update hyp_td_waveform_v2.py

--- hyp_td_waveform_v2.py
+++ hyp_td_waveform_v2.py
@@ -6,7 +6,7 @@ from rr_method1 import solve_rr
 from getx_v2 import get_x, get_x_PN
 from fn import rx, phitx, phiv, rtx
 from hypmik3pn import get_u, get_u_v2
-
+from eval_max import get_max, Fomg
 
 from pycbc.types import TimeSeries
 
@@ -15,7 +15,7 @@ Mpc=1e6*pc
 
 
 def get_hyp_waveform(M,q,et0,b,delta_t,inc,distance,order):
-    ti=M/100
+    ti=M/50
     eta=q/(1+q)**2
     time=M*tsun
     dis=M*dsun
@@ -31,25 +31,35 @@ def get_hyp_waveform(M,q,et0,b,delta_t,inc,distance,order):
     y0=[et0,n0,u_i]
     sol=solve_rr(eta,b,y0,t_i,t_f,t_arr)
     u_method1=sol[2]
-    
+    earr=sol[0]
+    narr=sol[1]
     step=len(tarr)
     hp_arr=np.zeros(step)
     hx_arr=np.zeros(step)
+    X=np.zeros(step)
+    Y=np.zeros(step)
     for i in range(step):
-        et=et0
+        et=earr[i]
         u=u_method1[i]
-        x=get_x(et0,eta,b,order)[0]   
+        x=get_x(et,eta,b,order)[0]   
         phi=phiv(eta,et,u,x,order)
         r1=rx(eta,et,u,x,order)
         z=1/r1
         phit=phitx(eta,et,u,x,order)
         rt=rtx(eta,et,u,x,order)
         phi=phiv(eta,et,u,x,order)
+        phi=phiv(eta,et,u,x,order)
+        r1=rx(eta,et,u,x,order)
+        X[i]=r1*cos(phi)
+        Y[i]=r1*sin(phi)
         hp_arr[i]=(-eta*(sin(inc)**2*(z-r1**2*phit**2-rt**2)+(1+cos(inc)**2)*((z
         +r1**2*phit**2-rt**2)*cos(2*phi)+2*r1*rt*phit*sin(2*phi))))
         hx_arr[i]=(-2*eta*cos(inc)*((z+r1**2*phit**2-rt**2)*sin(2*phi)-2*r1*rt*phit*cos(2*phi)))
     Hp=TimeSeries(hp_arr/scale, delta_t=delta_t, epoch=-ti)
     Hx=TimeSeries(hx_arr/scale, delta_t=delta_t, epoch=-ti)
+    #dimless_peak=get_max(eta,b,et0)
+    #peak=dimless_peak/(2*np.pi*time)
+    #return Hp-Hp[0],Hx-Hx[0], X, Y, peak
     return Hp-Hp[0],Hx-Hx[0]
 
     
